5663db81a3a262ddb651c42d27259fbc68282187
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2026-01-10 16:28:33 -0600

    feat: add Toggle component

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,66 @@
+import { type Accessor, type Setter, type JSX } from "solid-js";
+import { styled } from "solid-styled-components";
+
+const ToggleBase = styled.label`
+    position: relative;
+    display: inline-block;
+    width: 42px;
+    height: 26px;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    margin-right: 0.2rem;
+    border: 1px solid transparent;
+    &:focus-within {
+        outline: default;
+    }
+`;
+
+const Slider = styled.span`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: var(--toggle-inactive);
+    border-radius: 5px;
+    transition: 0.2s;
+    
+    ${(props: { checked: boolean }) => props.checked ? `
+        background-color: var(--primary-color);
+    ` : ""}
+    &:before {
+        position: absolute;
+        content: "";
+        height: 16px;
+        width: 16px;
+        left: 4px;
+        bottom: 5px;
+        background-color: white;
+        border-radius: 5px;
+        transition: 0.2s;
+        ${(props: { checked: boolean }) => props.checked ? `
+            transform: translateX(16px);
+        ` : ""}
+    }
+`;
+
+interface ToggleProps { 
+    checked: Accessor<boolean|undefined>|Accessor<boolean>; 
+    setChecked: Setter<boolean|undefined>|Setter<boolean>; 
+    onChange?: JSX.ChangeEventHandlerUnion<HTMLInputElement, Event>, 
+    disabled?: boolean; 
+};
+
+const Toggle = ({ checked, setChecked, onChange, disabled }: ToggleProps) => {
+    return (
+        <ToggleBase>
+            <input type="checkbox" checked={checked()} onChange={e => {
+                (setChecked as Setter<boolean>)(e.currentTarget.checked);
+                (onChange as Function | undefined)?.(e);
+            }} disabled={disabled} />
+            <Slider checked={checked()??false} />
+        </ToggleBase>
+    )
+};
+
+export default Toggle;

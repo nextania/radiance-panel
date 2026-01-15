5b94cbaf956cd9f99cd454b3cc0193862dc92f89
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2026-01-15 12:03:21 -0600

    feat: load real host data

--- src/pages/Hosts.tsx
+++ src/pages/Hosts.tsx
@@ -1,15 +1,23 @@
-import { createSignal } from 'solid-js'
 import { Button } from "../components/Button.tsx";
 import Host from "../components/Host.tsx";
-import { styled } from "solid-styled-components";
 import { FiPlus } from "solid-icons/fi";
 import { useTranslate } from "../i18n";
-import { Container, Content, Header, HeadingLarge, Section, SubheadingLarge } from '../components/Layout.tsx';
-
-
+import { Container, Content, Header, HeadingLarge, Section, SubheadingLarge } from "../components/Layout.tsx";
+import { useClient } from "../context.tsx";
+import { createResource, For, Show } from "solid-js";
 
 const Hosts = () => {
   const t = useTranslate();
+  const client = useClient();
+  
+  const [hosts] = createResource(async () => {
+    const hostsData = await client.getHosts();
+    return Object.entries(hostsData).map(([id, host]) => ({
+      id,
+      ...host
+    })).sort((a, b) => a.domains[0].localeCompare(b.domains[0]));
+  });
+  
   return (
     <>
       <Container>
@@ -23,12 +31,17 @@ const Hosts = () => {
           </Section>
         </Header>
         <Content>
-          <Host hostname="example.com" enabled={true} tls={true} />
-          <Host hostname="cloud.example.com" enabled={true} tls={true} />
-          <Host hostname="demo.example.com" enabled={false} tls={false} />
-          <Host hostname="dns.example.com" enabled={true} tls={true} />
-          <Host hostname="panel.example.com" enabled={true} tls={true} />
-          <Host hostname="sso.example.com" enabled={true} tls={true} />
+          <Show when={!hosts.loading} fallback={<div>Loading...</div>}>
+            <For each={hosts()}>
+              {(host) => (
+                <Host 
+                  hostname={host.domains[0] || "Unknown"} 
+                  enabled={host.enabled} 
+                  tls={!!host.tls_cert_id} 
+                />
+              )}
+            </For>
+          </Show>
         </Content>
       </Container>
     </>
